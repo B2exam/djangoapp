4236cb50f59778e9d04fc4f6abdeaab2f96e8b97
yagouam <56433614+yagouam@users.noreply.github.com>
2024-04-22 13:09:37 +0200

    Update forms.py

--- movies/forms.py
+++ movies/forms.py
@@ -1,3 +1,13 @@
 from django.forms import ModelForm
 
 from .models import Movie
+
+class MovieForm(ModelForm):
+
+  """
+  Formulario de creación de películas
+  """
+
+  class Meta:
+      model = Movie
+      fields = ('title', )
